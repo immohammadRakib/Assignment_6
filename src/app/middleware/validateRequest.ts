dd06948e6e391c7835bc8e6f0e3b522ed26f794a
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-09-07 23:00:30 +0600

    validation middleware update

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -4,9 +4,13 @@ import { catchAsync } from "../utils/catchAsync";
 
 export const validateRequest = (zodSchema: z.ZodObject) => {
   return catchAsync((req: Request, res: Response, next: NextFunction) => {
-    const payload = req.body ?? {};
+    // const payload = req.body ?? {};
 
-    const result = zodSchema.safeParse(payload);
+    // const result = zodSchema.safeParse(payload);
+    const result = zodSchema.safeParse({
+      body: req.body,
+      params: req.params,
+    });
 
     if (!result.success) {
       console.log(result.error);
