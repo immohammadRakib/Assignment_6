e27519b0898cdb5ff7310d67cfd2397c5edf07a8
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-09-07 18:35:05 +0600

    area get

--- src/app/module/grid/grid.service.ts
+++ src/app/module/grid/grid.service.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "../../../generated/prisma/browser";
 import { prisma } from "../../lib/prisma";
 
 const createPowerAuthorityInDB = async (payload: any) => {
@@ -41,6 +42,68 @@ const softDeleteAreaFromDB = async (areaId: string) => {
 };
 
 
+
+
+const getAllZonesFromDB = async () => {
+  return await prisma.distributionZone.findMany({
+    include: {
+      powerAuthority: true, // জোনের সাথে কোন অথরিটি তা দেখাবে
+    },
+  });
+};
+
+// ২. সব সাবস্টেশন তুলে আনা (জোন ও অথরিটিসহ)
+const getAllSubstationsFromDB = async () => {
+  return await prisma.substation.findMany({
+    include: {
+      zone: {
+        include: { powerAuthority: true }
+      },
+    },
+  });
+};
+
+// ৩. সব ফিডার লাইন তুলে আনা
+const getAllFeedersFromDB = async () => {
+  return await prisma.feeder.findMany({
+    include: {
+      substation: true,
+      areas: {
+        where: { isDeleted: false } // শুধুমাত্র ডিলিট না হওয়া এরিয়া দেখাবে
+      }
+    },
+  });
+};
+
+// ৪. সব এরিয়া তুলে আনা (রোল ভিত্তিক ফিল্টারিং এর সুবিধার্থে)
+const getAllAreasFromDB = async (query: any) => {
+  const { searchTerm, priority } = query;
+  const whereConditions: Prisma.AreaWhereInput = { isDeleted: false };
+
+  // সার্চ টার্ম থাকলে নাম দিয়ে ফিল্টার
+  if (searchTerm) {
+    whereConditions.name = {
+      contains: searchTerm,
+      mode: "insensitive",
+    };
+  }
+
+  // প্রায়োরিটি (VIP/NORMAL) ফিল্টার
+  if (priority) {
+    whereConditions.priority = priority; 
+  }
+
+  return await prisma.area.findMany({
+    where: whereConditions,
+    include: {
+      feeder: {
+        include: { substation: true }
+      },
+    },
+  });
+};
+
+
 export const GridServices = {
   createPowerAuthorityInDB,
   createZoneInDB,
@@ -48,4 +111,8 @@ export const GridServices = {
   createFeederInDB,
   createAreaInDB,
   softDeleteAreaFromDB,
+  getAllZonesFromDB,
+  getAllSubstationsFromDB,
+  getAllFeedersFromDB,
+  getAllAreasFromDB,
 };
