adc98556e78ffc0d39a661ffc00ed354bb5ab802
Md Abdur Rahman Rakib <rakiblp6@gmail.com>
2026-09-07 18:36:18 +0600

    area get api controller create

--- src/app/module/grid/grid.controller.ts
+++ src/app/module/grid/grid.controller.ts
@@ -67,6 +67,49 @@ const softDeleteArea = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+
+
+const getAllZones = catchAsync(async (req: Request, res: Response) => {
+  const result = await GridServices.getAllZonesFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Distribution Zones fetched successfully",
+    data: result,
+  });
+});
+
+const getAllSubstations = catchAsync(async (req: Request, res: Response) => {
+  const result = await GridServices.getAllSubstationsFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Substations fetched successfully",
+    data: result,
+  });
+});
+
+const getAllFeeders = catchAsync(async (req: Request, res: Response) => {
+  const result = await GridServices.getAllFeedersFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Feeder lines fetched successfully",
+    data: result,
+  });
+});
+
+const getAllAreas = catchAsync(async (req: Request, res: Response) => {
+  const result = await GridServices.getAllAreasFromDB(req.query);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Areas fetched successfully",
+    data: result,
+  });
+});
+
+
 export const GridControllers = {
   createPowerAuthority,
   createZone,
@@ -74,4 +117,8 @@ export const GridControllers = {
   createFeeder,
   createArea,
   softDeleteArea,
+  getAllZones,
+  getAllSubstations,
+  getAllFeeders,
+  getAllAreas,
 };
